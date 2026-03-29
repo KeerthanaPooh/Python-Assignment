e836bf72a87f919d95f661c5c2bf5d0e30e00384
Keerthana taruni <155227492+KeerthanaPooh@users.noreply.github.com>
2026-03-29 20:33:17 +0530

    Create assignment5.py with assignment details
    Added documentation for Assignment 5 covering pattern printing and nested loops.

---
+++ Assignment-5/assignment5.py
@@ -0,0 +1,13 @@
+# Assignment 5
+
+This assignment covers pattern printing and nested loop concepts.
+
+## Concepts Covered:
+- For loop inside for loop
+- For loop inside while loop
+- While loop inside while loop
+- While loop inside for loop
+
+## Programs Included:
+- Pattern printing programs
+- Nested loop examples
