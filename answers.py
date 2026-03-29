ac309e0418e638918193e89e1256c2685a8ad549
Keerthana taruni <155227492+KeerthanaPooh@users.noreply.github.com>
2026-03-29 19:31:08 +0530

    Create answers.py

---
+++ answers.py
@@ -0,0 +1,31 @@
+# Python Assignment Answers
+
+def show_answers():
+    print("1. Python is a high-level, interpreted programming language.\n")
+
+    print("2. Key Features:")
+    print("- Easy to learn")
+    print("- Readable syntax")
+    print("- Object-oriented")
+    print("- Large libraries\n")
+
+    print("3. Python 2 vs Python 3:")
+    print("Python 2 is outdated, Python 3 is modern and widely used.\n")
+
+    print("4. Applications:")
+    print("Web development, AI/ML, Data Science, Automation\n")
+
+    print("5. PEP 8:")
+    print("It is a style guide for writing clean Python code.\n")
+
+    print("6. Developed by Guido van Rossum in 1991.\n")
+
+    print("7. Dynamically typed:")
+    print("Variable types are decided at runtime.\n")
+
+    print("8. Interpreter:")
+    print("Python executes code line by line.\n")
+
+
+# Run the function
+show_answers()
